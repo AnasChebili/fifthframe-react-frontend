55c2c340001e1132453401822407187ba1f103dd
Anas Chebili <anaschebili@gmail.com>
2025-01-28 03:47:36 +0100

    set up shadcn

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { Button } from "@/components/ui/button";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -9,7 +10,10 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <React.Fragment>
-      <div>Hello "__root"!</div>
+      <div>
+        Hello "__root"!
+        <Button className="h-40">click</Button>
+      </div>
       <Outlet />
       <TanStackRouterDevtools />
     </React.Fragment>
