2e15e713c917f5aef848c680bc0f7760c33d4cf3
Anas Chebili <anaschebili@gmail.com>
2025-05-04 14:12:46 +0200

    added recommendations drawer component

---
+++ src/components/recommendations-drawer.tsx
@@ -0,0 +1,9 @@
+import { Dispatch, SetStateAction } from "react";
+
+export const recommendationsDrawer = ({
+  open,
+  setOpen,
+}: {
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+}) => {};
