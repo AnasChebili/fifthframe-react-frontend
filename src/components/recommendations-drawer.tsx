7343bef328edda883b2baa4add27dd38751af2a1
Anas Chebili <anaschebili@gmail.com>
2025-05-09 13:25:41 +0200

    added error ui display

--- src/components/recommendations-drawer.tsx
+++ src/components/recommendations-drawer.tsx
@@ -3,6 +3,7 @@ import { Drawer, DrawerContent } from "./ui/drawer";
 import { Thinking } from "./thinking";
 import { RecommendationsContainer } from "./recommendations-container";
 import { useGetTrendingMovies } from "@/hooks/use-movie";
+import { AlertCircle } from "lucide-react";
 
 export const RecommendationsDrawer = ({
   open,
@@ -15,15 +16,22 @@ export const RecommendationsDrawer = ({
   return (
     <Drawer open={open} onOpenChange={setOpen}>
       <DrawerContent className=" min-h-[50%] dark">
-        {!error && moviesPages ? (
+        {error ? (
+          <div className="flex flex-col items-center justify-center p-6 text-center">
+            <AlertCircle className="w-6 h-6 mb-2 text-red-500" />
+            <p className="text-red-500">
+              Sorry, we couldn't load recommendations
+            </p>
+          </div>
+        ) : !moviesPages ? (
+          <Thinking />
+        ) : (
           <section className="p-4">
             <h1 className="mb-5 text-2xl text-white">
               Based On Your Description
             </h1>
             <RecommendationsContainer />
           </section>
-        ) : (
-          <Thinking />
         )}
       </DrawerContent>
     </Drawer>
