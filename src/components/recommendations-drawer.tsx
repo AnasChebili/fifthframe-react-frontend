abb9564b36483dddeeb8cad54cb147208a3ac9af
Anas Chebili <anaschebili@gmail.com>
2025-05-09 13:20:46 +0200

    added conditional rendering based on data

--- src/components/recommendations-drawer.tsx
+++ src/components/recommendations-drawer.tsx
@@ -2,6 +2,7 @@ import { Dispatch, SetStateAction } from "react";
 import { Drawer, DrawerContent } from "./ui/drawer";
 import { Thinking } from "./thinking";
 import { RecommendationsContainer } from "./recommendations-container";
+import { useGetTrendingMovies } from "@/hooks/use-movie";
 
 export const RecommendationsDrawer = ({
   open,
@@ -10,15 +11,20 @@ export const RecommendationsDrawer = ({
   open: boolean;
   setOpen: Dispatch<SetStateAction<boolean>>;
 }) => {
+  const { data: moviesPages, error } = useGetTrendingMovies();
   return (
     <Drawer open={open} onOpenChange={setOpen}>
       <DrawerContent className=" dark">
-        <section className="p-4">
-          <h1 className="mb-5 text-2xl text-white">
-            Based On Your Description{" "}
-          </h1>
-          <RecommendationsContainer />
-        </section>
+        {!error && moviesPages ? (
+          <section className="p-4">
+            <h1 className="mb-5 text-2xl text-white">
+              Based On Your Description
+            </h1>
+            <RecommendationsContainer />
+          </section>
+        ) : (
+          <Thinking />
+        )}
       </DrawerContent>
     </Drawer>
   );
