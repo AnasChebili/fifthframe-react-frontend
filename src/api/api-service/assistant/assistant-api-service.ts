ea63b0cd73c2ce3dc7521bc8f7e75d9d0a188f11
Anas Chebili <anaschebili@gmail.com>
2025-05-12 12:47:03 +0200

    created assistant api function

--- src/api/api-service/assistant/assistant-api-service.ts
+++ src/api/api-service/assistant/assistant-api-service.ts
@@ -1,1 +1,12 @@
-export class AssistantApiSerice {}
+import { assistantApi } from "@/api/axios";
+
+export class AssistantApiSerice {
+  static async postAsisstantMessage({
+    message,
+  }: {
+    message: string;
+  }): Promise<string> {
+    const { data } = await assistantApi.post("/", { message });
+    return data;
+  }
+}
