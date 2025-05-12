e7df98d7e7fc2859e8a54175fc19a8bac39c4bcd
Anas Chebili <anaschebili@gmail.com>
2025-05-12 11:56:12 +0200

    changed to the correct router devtools dependency; put devtools back

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import fifthframeLogo from "../assets/fifthframe-logo.svg";
@@ -27,9 +27,9 @@ function RootComponent() {
             <Outlet />
           </div>
         </div>
-        {/* 
+
         <TanStackRouterDevtools />
-        <ReactQueryDevtools /> */}
+        <ReactQueryDevtools />
       </QueryClientProvider>
     </React.Fragment>
   );
